a377209010e4948836e876492574b9a53b7e173d
Haifeng Li <hfli@thoughtworks.com>
2021-01-19 16:13:06 +0800

    add

---
+++ src/main/java/com/riguz/shared/DemoRepository.java
@@ -0,0 +1,10 @@
+package com.riguz.shared;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DemoRepository {
+    public List<String> getNames() {
+        return Arrays.asList("Foo", "Bar");
+    }
+}
